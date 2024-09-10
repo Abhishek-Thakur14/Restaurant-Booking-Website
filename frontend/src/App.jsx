6a148bf0999344f64e3e947fcb8cec69062398ce
Abhishek kumar <thecode.abhi@gmail.com>
2024-09-11 01:47:54 +0530

    Succesfully completed the Server And Client side functionalities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
+import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Success from './pages/Success';
-import './App.css'
+
 const App = () => {
   return (
     <>
-      <Router>
-        <Routes>
+    <Router>
+         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/success' element={<Success/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Routes>
         <Toaster/>
-      </Router>
+       </Router>
     </>
   )
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ABHISHEK KUMAR</div>
+          <div className="right">
+            <p>09, Jaynagar, Madhubani, Bihar 847226</p>
+            <p>Open: 10:00 AM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Abhishek The Coder </p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Abhishek The Coder</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
